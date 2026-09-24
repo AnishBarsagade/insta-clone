77f6a732bc0171e7826f7c48888a9b565fdc3f68
Anish <anish0159590@gmail.com>
2026-09-24 21:34:04 +0530

    Add ImageKit image upload

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const authRouter = require("./Routes/auth.routes");
+const postRouter = require("./Routes/post.routes");
 
 app.use(express.json());
 app.use(cookieParser());
+// prefixes
 app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
 module.exports = app;
